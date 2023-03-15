514b371c4040a1a0fa2057505d5ddfbfc7ce2e79
Blvke <tafaravutuza@gmail.com>
2023-03-15 09:59:19 +0200

    Inheritance using getters and setters

---
+++ Inheritance2/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance2
+{
+    class Employee : Person
+    {
+        public string ID { get; set; }
+        public void IntroduceSelf()
+        {
+            Console.WriteLine("Hello my name is" + Name + "and my id is " + ID);
+        }
+    }
+}
---
+++ Inheritance2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inheritance2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            e.Name = "Tafara";
+            e.Age = 25;
+            e.ID = "1234556h6";
+
+            e.IntroduceSelf();
+        }
+    }
+}
