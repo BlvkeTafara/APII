19f9c672faded46d62caf1704c4a0d3a7f84ea18
Blvke <tafaravutuza@gmail.com>
2023-03-17 17:05:05 +0200

    Can you help me paIItem apo i am having an error  of which paVideo tutorial yangu there is no error

---
+++ Inheritance2/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance2
+{
+    class Student
+    {
+        public int GradeLevel { get; set; }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        interface IItem
+        {
+            string name { get; set; }
+            int goldvalue { get; set; }
+
+            void Equip();
+            void Sell(); 
+        }
+        class Sword : IItem
+        {
+            public string name { get; set; }
+            public int goldvalue { get; set; }
+
+            public Sword (string _name)
+            {
+                name = _name;
+                goldvalue = 100;
+            }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + "equipped");
+            }
+            public void sell ()
+            {
+                Console.WriteLine(name + "sold for" + goldvalue + "imaginary dollars");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Sword sword = new Sword("Sword of destiny");
+            sword.Equip();
+            sword.sell();
+            Console.ReadKey();
+        }
+    }
+}
